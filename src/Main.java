d56609e91e48bd923d25ca7cf4f269d6b85f8808
Omar Alzare <alzar3omar@gmail.com>
2024-12-04 16:17:51 +0300

    Edit main

--- src/Main.java
+++ src/Main.java
@@ -1,35 +1,39 @@
 import java.util.Scanner;
+
 public class Main {
+    static Scanner input = new Scanner(System.in);
+
     public static void main(String[] args) throws Exception {
-       Scanner input = new Scanner(System.in);
-       User user1 = new User();
-       
-       String id;
-       String password;
-       
-       System.out.print("\t**** Welcome To the Event Management System program ****\n\t--Please enter your ID and password\n");   //Missing Loop
-       id = input.nextLine();
-       password = input.nextLine();
-          
-       user1.login(id,password);
-       
-       char loginStatus=user1.getLoginStatus();
-       
-       switch(loginStatus){
-           case 'p':
-               System.out.println("\t**** participant menu ****");    //Missing Loop
-               break;
-           case 'o':
-               System.out.println("\t**** organizer menu ****");  //Missing Loop
-               break;
-           case 'a':
-               System.out.println("\t**** admin menu ****");  //Missing Loop
-               break;
-           default:
-               System.out.println("\t**** The id or password you typed is incorrect. ****");
-               break;
-       }
-       
-       input.close();
+        User user1 = new User();
+
+        String id;
+        String password;
+
+        System.out.print(
+                "\t**** Welcome To the Event Management System program ****\n\t--Please enter your ID and password\n"); // Missing
+                                                                                                                        // Loop
+        id = input.nextLine();
+        password = input.nextLine();
+
+        user1.login(id, password);
+
+        char loginStatus = user1.getLoginStatus();
+
+        switch (loginStatus) {
+            case 'p':
+                System.out.println("\t**** participant menu ****"); // Missing Loop
+                break;
+            case 'o':
+                System.out.println("\t**** organizer menu ****"); // Missing Loop
+                break;
+            case 'a':
+                System.out.println("\t**** admin menu ****"); // Missing Loop
+                break;
+            default:
+                System.out.println("\t**** The id or password you typed is incorrect. ****");
+                break;
+        }
+
+        input.close();
     }
 }
